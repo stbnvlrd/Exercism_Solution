5a8e7b62f64b1cf26cd1f28b4bf3496d043c25a0
stbnvlrd <estebanvelardegarcez@gmail.com>
2022-04-06 15:11:57 -0500

    Raindrops exercise solution

---
+++ Python/raindrops.py
@@ -0,0 +1,11 @@
+def convert(number):
+    result =''
+    if number%3 == 0:
+        result = result + 'Pling'
+    if number%5 == 0:
+        result = result + 'Plang'
+    if number%7 == 0:
+        result = result + 'Plong'
+    if not result:
+        result = str(number)
+    return result
