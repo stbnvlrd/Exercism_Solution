5c862c7e7e86cb0aa6943f7a6c7f959f0ebcc049
stbnvlrd <estebanvelardegarcez@gmail.com>
2022-06-07 12:21:30 -0500

    Acronym Exercise Solution

--- Ruby/acronym.rb
+++ Ruby/acronym.rb
@@ -1,21 +1,40 @@
 =begin
-Write your code for the 'Acronym' exercise in this file. Make the tests in
-`acronym_test.rb` pass.
+Write your code for the 'Anagram' exercise in this file. Make the tests in
+`anagram_test.rb` pass.
 
 To get started with TDD, see the `README.md` file in your
-`ruby/acronym` directory.
+`ruby/anagram` directory.
 =end
+class Anagram
+  def initialize(anagram)
+    @anagram = anagram.downcase.split("")
+    @letter_count = anagram.length
+  end
 
-class Acronym
-    def self.abbreviate(phrase)
-      acronym = ''
-      phrase = phrase.gsub('- ','')
-      phrase = phrase.gsub('-',' ')
-      phrase_words = phrase.split(' ')
-      phrase_words.each do |word|
-        acronym = acronym + word[0]
+  def match(words)
+    count = Array.new(words.length, 0)
+    result= []
+    words.each_with_index do |word, index|
+      @anagram.each do |letter|
+        if word.downcase.include? letter
+          count[index] = count[index] + 1
+        end
       end
-    acronym = acronym.upcase
+    end
+    count.each_with_index do |word, index|
+      if word == @letter_count and words[index].length == @letter_count and words[index].downcase != @anagram.join("")
+        result << words[index]
+      end
+    end
+  result.each do |word|
+    @anagram.each do |letters|
+      if @anagram.count(letters) != word.downcase.count(letters)
+        result.delete(word)
+      end
     end
   end
+  return result
+  end
+end
     
+  
