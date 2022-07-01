6291033fc712b6728dc421ce13a5e4faf8399d9c
stbnvlrd <estebanvelardegarcez@gmail.com>
2022-07-01 12:16:30 -0500

    Pig Latin Exercise Solution

--- Python/pig_latin.py
+++ Python/pig_latin.py
@@ -1,31 +1,37 @@
 CONSONANTS = ["b","c","d","f","g","h","j","k","l","m","n","p","q","r","s","t","v","w","x","z"]
 VOWELS = ["a","e","i","o","u","y"]
 def translate(text):
+    text_separated = text.split()
+    word_con = ""
+    final_list = []
+    for words in text_separated:
+        vowel_start = False
+        for vowel in VOWELS:
+            if words[0] == vowel:
+                vowel_start = True
+        
+        if (words[0:2] != "xr" and words[0:2] != "yt" and vowel_start != True) or (words[0] == "y" and words[1] != "t"):
+            if words[1:3] == "qu":
+                word_con = words[3:len(words)] + words[0:3]
+            elif words[0:2] == "qu":
+                word_con = words[2:len(words)] + words[0:2]
+            else:
+                
+                position = 0
+                vowel_pos = 0
+                while vowel_pos == 0:
+                    for vowel in VOWELS:
+                        if words[position] == vowel:
+                            vowel_pos = position
+                            print(vowel_pos)
+                            break
+                    position = position + 1
+                word_con = words[vowel_pos:len(words)] + words[0:vowel_pos]
     
-    vowel_start = False
-    for vowel in VOWELS:
-        if text[0] == vowel:
-            vowel_start = True
-    
-    if (text[0:2] != "xr" and text[0:2] != "yt" and vowel_start != True) or (text[0] == "y" and text[1] != "t"):
-        if text[1:3] == "qu":
-            text = text[3:len(text)] + text[0:3]
-        elif text[0:2] == "qu":
-            text = text[2:len(text)] + text[0:2]
+        
         else:
-            
-            position = 0
-            vowel_pos = 0
-            while vowel_pos == 0:
-                for vowel in VOWELS:
-                    if text[position] == vowel:
-                        vowel_pos = position
-                        print(vowel_pos)
-                        break
-                position = position + 1
-            text = text[vowel_pos:len(text)] + text[0:vowel_pos]
+            word_con = words
+        word_con = word_con + "ay"
+        final_list.append(word_con)
 
-    
-            
-    text = text + "ay"    
-    return text
+    return " ".join(final_list)
