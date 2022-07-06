597d032e70b39a6d1ac5420c4d15c84bdae0ecdb
stbnvlrd <estebanvelardegarcez@gmail.com>
2022-07-06 12:16:30 -0500

    Wordy Exercise unknown operation and syntax error

--- Python/wordy.py
+++ Python/wordy.py
@@ -1,12 +1,25 @@
 def answer(question):
     equation = question[8:-1]
     split_equation = equation.split()
-    if split_equation[1] == "plus":
-        return(int(split_equation[0]) + int(split_equation[2]))
-    elif split_equation[1] == "minus":
-        return(int(split_equation[0]) - int(split_equation[2]))
-    elif split_equation[1] == "multiplied":
-        return(int(split_equation[0]) * int(split_equation[3]))
-    elif split_equation[1] == "divided":
-        return(int(split_equation[0]) / int(split_equation[3]))
-
+    if len(split_equation) == 1:
+        return(int(split_equation[0]))
+    if len(split_equation) == 0:
+        raise ValueError("syntax error")
+    elif len(split_equation) > 4 and split_equation[0].isdecimal():
+        return ("multiple")
+    elif len(split_equation) == 2:
+        raise ValueError("syntax error")
+    elif len(split_equation) > 2 and len(split_equation) < 5:
+        if split_equation[1] == "plus":
+            return(int(split_equation[0]) + int(split_equation[2]))
+        elif split_equation[1] == "minus":
+            return(int(split_equation[0]) - int(split_equation[2]))
+        elif split_equation[1] == "multiplied":
+            return(int(split_equation[0]) * int(split_equation[3]))
+        elif split_equation[1] == "divided":
+            return(int(split_equation[0]) / int(split_equation[3]))
+        else:
+            raise ValueError("unknown operation")
+    else:
+        raise ValueError("unknown operation")
+    pass
