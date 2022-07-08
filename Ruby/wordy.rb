a0e32d4369e179fb3944c043ad2f0e5abdae9cf3
stbnvlrd <estebanvelardegarcez@gmail.com>
2022-07-08 12:16:30 -0500

    Wordy Exercise First Operation

--- Ruby/wordy.rb
+++ Ruby/wordy.rb
@@ -1,44 +1,31 @@
 class WordProblem
-    def initialize(phrase)
-      @phrase = phrase
+  def initialize(phrase)
+    phrase = phrase.slice!(8..-2)
+    phrase = phrase.gsub(' by', ' ')
+    @phrase = phrase.split(' ')  
+    # puts @phrase
+  end
+
+  def answer
+    case @phrase[1]
+    when "plus"
+      result = @phrase[0].to_i + @phrase[2].to_i 
+    when "divided"
+      result = @phrase[0].to_i / @phrase[2].to_i
+    when "multiplied"
+      result = @phrase[0].to_i * @phrase[2].to_i
+    when "minus"
+      result = @phrase[0].to_i - @phrase[2].to_i
     end
-  
-    def answer
-      number_text = @phrase.delete "^0-9", "^ "
-      numbers = number_text.split(" ")
-    
-      target = 'plus'
-      sz = target.size
-      plus = (0..@phrase.size-sz).select { |i| @phrase[i,sz] == target }
-
-      target = 'minus'
-      sz = target.size
-      minus = (0..@phrase.size-sz).select { |i| @phrase[i,sz] == target }
-
-      target = 'multiplied'
-      sz = target.size
-      multiplied = (0..@phrase.size-sz).select { |i| @phrase[i,sz] == target }
-
-      target = 'divided'
-      sz = target.size
-      divided = (0..@phrase.size-sz).select { |i| @phrase[i,sz] == target }
-    
-      puts plus
-      puts minus
-      puts multiplied
-      puts divided
-      puts numbers
-
-      if multiplied != nill
-        
-    end
+    return result
+  end
 end
     
   problem = WordProblem.new("What is 1 plus 1 plus 1?")
   puts problem.answer
 
-  problem = WordProblem.new("What is 10 divided 2 plus 4?")
+  problem = WordProblem.new("What is 10 divided by 2 plus 1?")
   puts problem.answer
 
-  problem = WordProblem.new("What is 1 minus 1 minus 1?")
+  problem = WordProblem.new("What is 1 minus 1 plus 1?")
   puts problem.answer
