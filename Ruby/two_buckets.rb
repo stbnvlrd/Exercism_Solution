ee85dc71c5d7ca464ca473b6f37a3411d19dcc8f
stbnvlrd <estebanvelardegarcez@gmail.com>
2022-07-12 16:22:18 -0500

    Two Buckets Big bucket to small bucket

--- Ruby/two_buckets.rb
+++ Ruby/two_buckets.rb
@@ -1,3 +1,4 @@
+
 class TwoBucket
   def initialize(small_bucket, big_bucket, desired_volumen, full_bucket)
     @small_bucket = small_bucket
@@ -5,7 +6,7 @@ class TwoBucket
     @desired_volumen = desired_volumen
     @full_bucket = full_bucket
   end
-  
+    
   def moves
     count = 0
     small_bucket_remaining = 0
@@ -19,26 +20,63 @@ class TwoBucket
         if big_bucket_remaining > @big_bucket then
           big_bucket_remaining -= @big_bucket
         end
-        
-        puts big_bucket_remaining
-        puts small_bucket_remaining
-        puts @desired_volumen
+          
+        # puts big_bucket_remaining
+        # puts small_bucket_remaining
+        # puts @desired_volumen
+        @other_bucket = small_bucket_remaining
       end
     else
       while small_bucket_remaining != @desired_volumen do
-        if big_bucket_remaining = @big_bucket
-        small_bucket_remaining = @small_buckets
-        big_bucket_remaining -= small_bucket_remaining
-        while small_bucket_remaining > @small_bucket do
-          small_bucket_remaining -= @small_bucket
+        if big_bucket_remaining == 0 then
+          big_bucket_remaining = @big_bucket
+          count += 1
+        elsif big_bucket_remaining + small_bucket_remaining < @small_bucket then
+          small_bucket_remaining = big_bucket_remaining
+          big_bucket_remaining = 0
+          count += 1
+        elsif big_bucket_remaining + small_bucket_remaining > @small_bucket then
+          big_bucket_remaining = big_bucket_remaining - @small_bucket + small_bucket_remaining
+          small_bucket_remaining = @small_bucket
+          count += 1
+          
+        elsif small_bucket_remaining == @small_bucket then
+          small_bucket_remaining = 0
+          count += 1
         end
-        count += 2
+        puts big_bucket_remaining
+        puts small_bucket_remaining
+        @other_bucket = big_bucket_remaining
       end
     end
     return count
   end
+  
+  def goal_bucket
+    return @full_bucket
+  end
+  
+  def other_bucket
+    return @other_bucket
+  end
 end
+  
 
 
 subject = TwoBucket.new(3, 5, 1, 'one')
+puts subject.moves
+
+subject = TwoBucket.new(3, 5, 1, 'two')
+puts subject.moves
+
+subject = TwoBucket.new(7, 11, 2, 'one')
+puts subject.moves
+
+subject = TwoBucket.new(7, 11, 2, 'two')
+puts subject.moves
+
+subject = TwoBucket.new(1, 3, 3, 'two')
+puts subject.moves
+
+subject = TwoBucket.new(2, 3, 3, 'one')
 puts subject.moves
