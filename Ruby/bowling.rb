2b7b49701db1bc8963e26089eab7846491ab99a8
stbnvlrd <estebanvelardegarcez@gmail.com>
2022-07-14 08:44:04 -0500

    Bowling Exercise Strike score

--- Ruby/bowling.rb
+++ Ruby/bowling.rb
@@ -1,31 +1,88 @@
 class Game
-    def initialize()
-      @final_score = 0
-      @last_roll = 0
-      @roll = 0
-      @last_last_roll = 0
-      @roll_count = 0  
-    end
-  
-    def roll(pin)
-      @last_last_roll = @last_roll
-      @last_roll = @roll
-      @roll = pin
+  def initialize()
+    @final_score = 0
+    @last_roll = 0
+    @roll = 0
+    @last_last_roll = 0
+    @roll_count = 0  
+  end
+
+  def roll(pin)
+    @last_last_roll = @last_roll
+    @last_roll = @roll
+    @roll = pin
+    if pin == 10 then
+      @roll_count += 2
+    else
       @roll_count += 1  
-      @final_score = @final_score + pin
-      if @roll_count % 2 != 0 and (@last_last_roll + @last_roll) == 10 then
-        @final_score = @final_score + pin
-      elsif @last_last_roll == 10 then 
-        @final_score = @final_score + pin
-      elsif @last_roll == 10 then
-        @final_score = @final_score + pin
-      end
     end
-  
-  
-    def score
-      @final_score
+    print "Turn "
+    puts @roll_count
+    if @roll_count <= 20 then
+      @final_score = @final_score + pin
+      puts @final_score
+    end
+    if @roll_count % 2 != 0 and (@last_last_roll + @last_roll) == 10 and @last_roll != 10 and @last_roll != 0 then
+      @final_score = @final_score + pin
+      print "spare "
+      puts @final_score
+    end
+    if @last_last_roll == 10 then 
+      @final_score = @final_score + pin
+      print "strike 2 "
+      puts @final_score
+    end
+    if @last_roll == 10 and @roll_count < 23 then
+      @final_score = @final_score + pin
+      print "strike 1 "
+      puts @final_score
     end
   end
-      
-      
+
+
+  def score
+    @final_score
+  end
+end
+  
+  
+  # puts "Game 1"    
+  # game = Game.new
+  # rolls = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 7, 3, 7]
+  # rolls.each { |pins| game.roll(pins) }
+  # puts game.score
+
+  # puts " "  
+  # puts "Game 2"    
+  # game = Game.new
+  # rolls = [10, 10, 10, 5, 3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+  # rolls.each { |pins| game.roll(pins) }
+  # puts game.score
+
+  # puts " "  
+  # puts "Game 3"  
+  # game = Game.new
+  # rolls = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 10, 7, 3]
+  # rolls.each { |pins| game.roll(pins) }
+  # puts game.score
+  
+  # puts " "  
+  # puts "Game 4"  
+  # game = Game.new
+  # rolls = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 10, 10, 10]
+  # rolls.each { |pins| game.roll(pins) }
+  # puts game.score
+
+  puts " "  
+  puts "Game 5"  
+  game = Game.new
+  rolls = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 7, 3, 10]
+  rolls.each { |pins| game.roll(pins) }
+  puts game.score
+  
+  puts " "  
+  puts "Game 6"  
+  game = Game.new
+  rolls = [5, 5, 10, 6, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+  rolls.each { |pins| game.roll(pins) }
+  puts game.score
