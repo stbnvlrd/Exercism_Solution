e367a7e247998f76cb0ca23e19ef16b779e7879d
stbnvlrd <estebanvelardegarcez@gmail.com>
2022-07-15 10:03:56 -0500

    Hamming Exercise Difference length error correction

--- Ruby/hamming.rb
+++ Ruby/hamming.rb
@@ -2,14 +2,19 @@
 class Hamming
   def self.compute(strand_a, strand_b)
     count = 0
-    for index in (0..strand_a.length) do
-      if strand_a[index] != strand_b[index] then
-        count += 1
+    if strand_a.length == strand_b.length then
+      for index in (0..strand_a.length) do
+        if strand_a[index] != strand_b[index] then
+          count += 1
+        end
       end
+    else
+      raise ArgumentError
     end
     return count
   end
 end
+        
 
   puts Hamming.compute('GGACTGAAATCTG', 'GGACTGAAATCTG')
 
