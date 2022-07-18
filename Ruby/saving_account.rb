56c8bb76e059fa189e50daf40c12d515a4e5882f
stbnvlrd <estebanvelardegarcez@gmail.com>
2022-07-18 12:27:51 -0500

    Saving Account Interest rate from account balance

---
+++ Ruby/saving_account.rb
@@ -0,0 +1,24 @@
+module SavingsAccount
+    def self.interest_rate(balance)
+      if balance < 0 then
+        return -3.213
+      elsif balance < 1000 then
+        return 0.5
+      elsif balance < 5000 then
+        return 1.621
+      elsif balance >= 5000 then
+        return 2.475
+      else
+        raise "Error in interest rate"
+      end
+    end
+  
+    def self.annual_balance_update(balance)
+      raise 'Please implement the SavingsAccount.annual_balance_update method'
+    end
+  
+    def self.years_before_desired_balance(current_balance, desired_balance)
+      raise 'Please implement the SavingsAccount.years_before_desired_balance method'
+    end
+  end
+ 
