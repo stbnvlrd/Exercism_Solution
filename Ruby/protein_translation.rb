55969e8556f4d9a5b34277e4f2992aa37b971d21
stbnvlrd <estebanvelardegarcez@gmail.com>
2022-07-17 21:05:26 -0500

    Protein Translation Strand separation in Codon and translation

--- Ruby/protein_translation.rb
+++ Ruby/protein_translation.rb
@@ -1,26 +1,38 @@
 class Translation
-    def self.of_codon(strand)
-      translated_strand = []
-      number = strand.length / 3
-      for index in (0..number) do
-        codon = strand[index*3..(index*6)-1]
-        if codon == 'AUG' then
-          translated_strand.append('Methionine')
-        elsif codon == 'UGG' then
-          translated_strand.append('Tryptophan')
-        elsif codon == 'UUU' or codon == 'UUC' then
-          translated_strand.append('Phenylalanine')
-        elsif codon == 'UUA' or codon == 'UUG' then
-          translated_strand.append('Leucine')
-        elsif codon == 'UCU' or codon == 'UCC' or codon == 'UCA' or codon == 'UCG' then
-          translated_strand.append('Serine')
-        elsif codon == 'UAA' or codon == 'UAG' or codon == 'UGA' then
-          translated_strand.append('STOP')
-        elsif codon == 'UAU' or codon == 'UAC' then
-          translated_strand.append('Tyrosine')
-        elsif codon == 'UGU' or codon == 'UGC' then
-          translated_strand.append('Cysteine')
-        end
-      end
-      return translated_strand.join(" ")
+  class InvalidCodonError < StandardError
+  end
+  
+  def self.of_codon(codon)
+    if codon == 'AUG' then
+      return 'Methionine'
+    elsif codon == 'UGG' then
+      return 'Tryptophan'
+    elsif codon == 'UUU' or codon == 'UUC' then
+      return 'Phenylalanine'
+    elsif codon == 'UUA' or codon == 'UUG' then
+      return 'Leucine'
+    elsif codon == 'UCU' or codon == 'UCC' or codon == 'UCA' or codon == 'UCG' then
+      return 'Serine'
+    elsif codon == 'UAA' or codon == 'UAG' or codon == 'UGA' then
+      return 'STOP'
+    elsif codon == 'UAU' or codon == 'UAC' then
+      return 'Tyrosine'
+    elsif codon == 'UGU' or codon == 'UGC' then
+      return 'Cysteine'
     end
+  end
+
+  def self.of_rna(strand)
+    translated_strand = []
+    number = strand.length / 3
+    for index in (0..number-1) do
+      codon_translated = Translation.of_codon(strand.slice(3*index, 3))
+      if codon_translated == "STOP" then
+        return translated_strand
+      else
+        translated_strand.append(codon_translated)
+      end
+    end
+  return translated_strand
+  end
+end
