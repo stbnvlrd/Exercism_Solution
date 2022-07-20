d802994659ffca0f3c21007feada139afca5310a
stbnvlrd <estebanvelardegarcez@gmail.com>
2022-07-20 00:39:57 -0500

    High Scores Personal best and 3 best

--- Ruby/high_scores.rb
+++ Ruby/high_scores.rb
@@ -15,19 +15,32 @@ class HighScores
     end
   end
 
+  def personal_best
+    return @list_scores.max
+  end
+
+  def personal_top_three
+    return @list_scores.max(3)
+  end
 end
 
 scores = [30, 50, 20, 70]
 score = HighScores.new(scores)
 puts score.scores
 puts score.latest
+puts score.personal_best
+puts score.personal_top_three
 
 scores = [100, 0, 90, 30]
 score = HighScores.new(scores)
 puts score.scores
 puts score.latest
+puts score.personal_best
+puts score.personal_top_three
 
 scores = [10, 30, 90, 30, 100, 20, 10, 0, 30, 40, 40, 70, 70]
 score = HighScores.new(scores)
 puts score.scores
 puts score.latest
+puts score.personal_best
+puts score.personal_top_three
