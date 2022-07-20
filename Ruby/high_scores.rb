e71ee065f2da9598f371be3d39ece3c9f81f8eee
stbnvlrd <estebanvelardegarcez@gmail.com>
2022-07-20 00:38:13 -0500

    High Scores Score list and last score in the list

--- Ruby/high_scores.rb
+++ Ruby/high_scores.rb
@@ -5,4 +5,29 @@ class HighScores
         @list_scores.append(score)
       end
     end
+
+    def scores
+      return @list_scores
+    end
+  
+    def latest
+      return @list_scores[-1]
+    end
+  end
+
 end
+
+scores = [30, 50, 20, 70]
+score = HighScores.new(scores)
+puts score.scores
+puts score.latest
+
+scores = [100, 0, 90, 30]
+score = HighScores.new(scores)
+puts score.scores
+puts score.latest
+
+scores = [10, 30, 90, 30, 100, 20, 10, 0, 30, 40, 40, 70, 70]
+score = HighScores.new(scores)
+puts score.scores
+puts score.latest
