92fdaf05b9017e1e34b4135d2a40f14fd6bcfb0b
stbnvlrd <estebanvelardegarcez@gmail.com>
2022-07-26 11:54:41 -0500

    Beer Song Other phrases

--- Ruby/beer_song.rb
+++ Ruby/beer_song.rb
@@ -10,6 +10,16 @@ class BeerSong
         else
           phrase = counter.to_s + " bottles of beer on the wall, " +  counter.to_s + " bottles of beer.\n"
         end
+        counter -= 1
+        if counter == 1 then
+          phrase += "Take one down and pass it around, " +  counter.to_s + " bottle of beer on the wall.\n"
+        elsif counter == 0 then
+          phrase += "Take it down and pass it around, no more bottles of beer on the wall.\n"
+        elsif counter == -1 then
+          phrase += "Go to the store and buy some more, 99 bottles of beer on the wall.\n"
+        else
+          phrase += "Take one down and pass it around, " +  counter.to_s + " bottles of beer on the wall.\n"
+        end
         whole_text += phrase + "\n"
       end
     return whole_text[0..-2]
