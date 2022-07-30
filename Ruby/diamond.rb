b502cc19aa4d95dc9451e4b0efb6caf3bdd64a7d
stbnvlrd <estebanvelardegarcez@gmail.com>
2022-07-30 15:13:15 -0500

    Diamond From A to selected letter triangle

---
+++ Ruby/diamond.rb
@@ -0,0 +1,30 @@
+class Diamond
+    @letters = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z']
+    def self.make_diamond(final_letter)
+      space = 1
+
+      lines = []
+      length = @letters.index(final_letter) + 1
+      puts "length: " + length.to_s
+      text = ""
+      for letter in ('A'..final_letter) do
+        if letter == 'A' then
+          indent_length = length - 1
+          text = " "*(length - 1) + letter + " "*(length - 1)
+        else
+          indent_length -= 1
+          text = " "*indent_length + letter + " "*space + letter + ''*indent_length
+          space += 2 
+        end
+        lines.append(text)
+        # puts text 
+      end
+      return lines.join("\n")
+    end
+  end
+  
+  
+  puts Diamond.make_diamond('A')
+  puts Diamond.make_diamond('C')
+  puts Diamond.make_diamond('E')
+  
