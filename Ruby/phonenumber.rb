550150b6aed401548e54e77c026f3c33f1fe6d56
stbnvlrd <estebanvelardegarcez@gmail.com>
2022-08-01 12:27:07 -0500

    Phonenumber Restricting output base on the NANP

--- Ruby/phonenumber.rb
+++ Ruby/phonenumber.rb
@@ -6,10 +6,27 @@ class PhoneNumber
     number.gsub!("-", "")
     number.gsub!(".", "")
     number.gsub!("+", "")
-    return number
+    if number[0] == "1" and number.length == 11
+        number = number[1..-1]
+    end
+    if number.length == 10
+      if number[0] == "0" or number[0] == "1" or number[3] == "0" or number[3] == "1"
+        return nil
+      else
+        return number 
+      end
+    else
+        return nil
+    end
   end
 end
 
 puts PhoneNumber.clean("(223) 456-7890")
 puts PhoneNumber.clean("223.456.7890")
 puts PhoneNumber.clean("223 456   7890   ")
+puts PhoneNumber.clean("123456789")
+puts PhoneNumber.clean("22234567890")
+puts PhoneNumber.clean("123-abc-7890")
+puts PhoneNumber.clean("123-@:!-7890")
+puts PhoneNumber.clean("+1 (223) 456-7890")
+puts PhoneNumber.clean("1 (223) 056-7890")
