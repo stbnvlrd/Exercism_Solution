9357884beebe1750f5a516e40722142737bbb8e4
stbnvlrd <estebanvelardegarcez@gmail.com>
2022-08-08 18:02:00 -0500

    Luhn Detect letters in input

--- Ruby/luhn.rb
+++ Ruby/luhn.rb
@@ -1,24 +1,36 @@
 class Luhn
-  def self.valid?(number)
-    sum = 0
-    number = number.gsub(' ','')
-    # puts number.length
-    for digit in (1..number.length) do
-    #   puts number[digit-1]
-      if digit%2 == 0 then
-        digit_result = 2 * number[digit-1].to_i
-      else
-        digit_result = number[digit-1].to_i
+    def self.valid?(number)
+      sum = 0
+      number = number.gsub(' ','')
+      
+      if number.length < 2 or number.gsub(/[^0-9]/, '') != number then
+        return false
       end
-      if digit_result >= 10 then
-        digit_result = digit_result - 9
+      # puts number.length
+      for digit in (1..number.length) do
+      #   puts number[digit-1]
+        if digit%2 == 0  and number.length%2 != 0 then
+          digit_result = 2 * number[digit-1].to_i
+        elsif digit%2 != 0  and number.length%2 == 0 then
+          digit_result = 2 * number[digit-1].to_i
+        else
+          digit_result = number[digit-1].to_i
+        end
+        if digit_result >= 10 then
+          digit_result = digit_result - 9
+        end
+        sum += digit_result
+        # puts sum 
       end
-      sum += digit_result
-      puts sum 
+      
+      return sum%10 == 0
     end
-    
-    return sum%10 == 0
   end
-end
+    
 
 puts Luhn.valid?("059")
+puts Luhn.valid?("055 444 286")
+puts Luhn.valid?("055 444 285")
+puts Luhn.valid?("0")
+puts Luhn.valid?("055-444-285")
+puts Luhn.valid?("0000 0")
